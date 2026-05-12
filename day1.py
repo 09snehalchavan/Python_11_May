218247a1986d362b91677af85615bcb8515537da
09snehalchavan <snehalkc09@gmail.com>
2026-05-12 16:59:42 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,16 @@
+# Simple Hello Program.
+
+print("Hello.. Python nice to meet you")
+
+print("You are so simple..")
+
+a = 10
+d = 20
+print(a)
+print("Addition = ",a+d)
+
+b = "Fortune"
+print(b)
+
+c=23.12
+print(c)
